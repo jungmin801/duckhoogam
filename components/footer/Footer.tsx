a4a81cb5e601ff00cff77ec4f3988169afba8358
문정민 <130200440+jungmin801@users.noreply.github.com>
2024-01-24 18:52:49 +0900

    Feat/footerComponent#12 [ footer UI 구현 ] (#13)
    * Style: top버튼 position 설정
    top버튼에 position을 주어 위치를 고정
    Related to:#1
    
    * Feat: footer 컴포넌트 생성
    페이지 하단에 들어갈 footer 컴포넌트 구현
    Resolves:#12

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import TopButton from "../buttons/TopButton";
+
+const Footer = () => {
+  return (
+    <footer className="bg-slate-100">
+      <div className="relative flex items-center justify-center h-20 max-width">
+        <h2 className="text-custom-gray-600">©Moon Jung Min</h2>
+        <TopButton />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
