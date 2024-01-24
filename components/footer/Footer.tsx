5bcebac3d7099c58d82c6e7980e754aafddc87fe
문정민 <130200440+jungmin801@users.noreply.github.com>
2024-01-24 18:57:48 +0900

    Style: footer 배경색 수정 (#14)
    배경색 수정

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import TopButton from "../buttons/TopButton";
 
 const Footer = () => {
   return (
-    <footer className="bg-slate-100">
+    <footer className="bg-white">
       <div className="relative flex items-center justify-center h-20 max-width">
         <h2 className="text-custom-gray-600">©Moon Jung Min</h2>
         <TopButton />
