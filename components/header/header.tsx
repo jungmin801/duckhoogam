537a58db62a9fd08ddee59f109fdad2ce78f3e95
Jungmin Moon <jm.moon0801@gmail.com>
2024-01-24 18:35:06 +0900

    Feat: header 컴포넌트 생성 페이지 상단의 header 컴포넌트 생성 Resolves:#10

---
+++ components/header/header.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import BaseButton from "../buttons/BaseButton";
+import Image from "next/image";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="max-width">
+      <div className="flex items-center justify-between py-4">
+        <h1 className="text-lg font-extraBold">DuckHooGam</h1>
+        <ul className="flex gap-4 ">
+          <li>
+            <Link
+              href={"/"}
+              className="px-5 py-2 font-bold text-white rounded-lg bg-custom-blue button"
+            >
+              <Image
+                src={"/asset/image/modify-white.svg"}
+                alt=""
+                width={20}
+                height={20}
+              />
+              Write
+            </Link>
+          </li>
+          <li>
+            <button className="px-3 font-bold button">
+              <Image
+                src="/asset/image/logout.svg"
+                alt=""
+                width={20}
+                height={20}
+              />
+              Logout
+            </button>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
