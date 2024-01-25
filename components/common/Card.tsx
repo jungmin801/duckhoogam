c1edeabbfb1a9ff9a19ff69b053f162db6fc59b2
Jungmin Moon <jm.moon0801@gmail.com>
2024-01-25 15:35:32 +0900

    Rename: common 폴더 생성 및 파일 이동 common폴더를 생성하고 파일 위치 이동 Related to: #2

--- components/common/Card.tsx
+++ components/common/Card.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const Card = () => {
   return (
-    <article className="w-96 rounded-xl">
+    <article className="bg-white w-96 rounded-xl">
       <Image
         src="/asset/image/bingbong.jpg"
         alt=""
@@ -14,16 +14,16 @@ const Card = () => {
         height={50}
         className="rounded-t-xl"
       />
-      <div className="pt-4 pb-6 px-6">
+      <div className="px-6 pt-4 pb-6">
         <Badge txt="Music" />
-        <h3 className="shorten2 font-bold my-3">
+        <h3 className="my-3 font-bold shorten2">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum,
           corrupti! Repudiandae, sit accusamus! Perferendis, nam eligendi. Vel,
           voluptate autem iure similique saepe, ipsum, laborum ut error soluta
           eos corporis temporibus?
         </h3>
         <AuthorInfo author="Moonie" createdAt="2024.01.22" />
-        <p className="shorten3 text-custom-gray-500 mt-4">
+        <p className="mt-4 shorten3 text-custom-gray-500">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
           corporis praesentium consequatur saepe? Beatae eius vitae ipsa maiores
           pariatur libero possimus, minus odit fuga deleniti ratione natus
