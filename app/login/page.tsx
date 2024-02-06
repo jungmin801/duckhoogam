b46a197267ec97740339948775fa6d0ca0c24ea6
문정민 <130200440+jungmin801@users.noreply.github.com>
2024-02-06 18:07:15 +0900

    Feat/login#25 [ 로그인, 로그아웃 구현 ] (#30)
    * Chore: 로그인, 로그아웃 아이콘 파일 추가
    로그인, 로그아웃 아이콘 파일 추가
    
    * Feat: 로그인, 로그아웃 기능 구현
    로그인, 로그아웃 기능 구현
    Resolves:#25

---
+++ app/login/page.tsx
@@ -0,0 +1,105 @@
+"use client";
+import React from "react";
+import BaseButton from "../../components/buttons/BaseButton";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { supabase } from "../../utils/supabaseClient";
+import { useRouter } from "next/navigation";
+
+type FormValues = {
+  userName: string;
+  email: string;
+  password: string;
+};
+const Login = () => {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
+  const defaultStyle =
+    "block w-full py-3 border-b border-solid border-custom-gray-300 focus:outline-none font-custom-rg";
+  const errorStyle = "border-custom-red";
+
+  const submitData = async (data) => {
+    try {
+      const { data: userData, error } = await supabase.auth.signInWithPassword({
+        email: data.email,
+        password: data.password,
+      });
+
+      if (error) {
+        throw new Error(error.message);
+      }
+
+      if (userData) {
+        const { data, error } = await supabase.auth.setSession({
+          access_token: userData.session.access_token,
+          refresh_token: userData.session.refresh_token,
+        });
+
+        router.push("/");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <main className="flex items-center max-width h-svh">
+      <div className="relative w-1/2 max-w-full p-20 m-auto bg-white min-h-[500px] shadow-xl">
+        <h2 className="mb-10 text-2xl text-center font-custom-hv">로그인</h2>
+        <form
+          onSubmit={handleSubmit(submitData)}
+          className="flex flex-col gap-4"
+        >
+          <label className="block w-full text-sm font-custom-bd">
+            이메일
+            <input
+              type="email"
+              className={`${defaultStyle} ${errors.email && errorStyle}`}
+              placeholder="example@gmail.com"
+              {...register("email", {
+                required: "이메일을 입력해주세요.",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "올바른 형식의 이메일을 입력해주세요.",
+                },
+              })}
+            />
+          </label>
+          {errors.email && (
+            <p className="text-xs text-custom-red">{errors.email.message}</p>
+          )}
+          <label className="block w-full text-sm font-custom-bd">
+            비밀번호
+            <input
+              type="password"
+              className={`${defaultStyle} ${errors.password && errorStyle}`}
+              placeholder="비밀번호를 입력해주세요."
+              {...register("password", {
+                required: "비밀번호를 입력해주세요.",
+                minLength: {
+                  value: 6,
+                  message: "6자 이상의 비밀번호를 입력하세요.",
+                },
+              })}
+            />
+          </label>
+          {errors.password && (
+            <p className="text-xs text-custom-red">{errors.password.message}</p>
+          )}
+          <div className="mt-8 text-center">
+            <BaseButton
+              isSubmit={true}
+              isFilled={true}
+              txt={"로그인하기"}
+              width={"w-full"}
+            />
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,37 +1,74 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { supabase } from "../../utils/supabaseClient";
 
 const Header = () => {
+  const [isLogin, setIsLogin] = useState<boolean>();
+
+  supabase.auth.onAuthStateChange((event, session) => {
+    if (event === "SIGNED_IN") setIsLogin(true);
+    else if (event === "SIGNED_OUT") {
+      setIsLogin(false);
+      [window.localStorage, window.sessionStorage].forEach((storage) => {
+        Object.entries(storage).forEach(([key]) => {
+          storage.removeItem(key);
+        });
+      });
+    }
+  });
+
+  const signOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.error("Error signing out:", error);
+    }
+  };
+
   return (
     <header className="bg-white ">
       <div className="flex items-center justify-between py-4 max-width">
         <h1 className="text-lg font-custom-hv">DuckHooGam</h1>
         <ul className="flex gap-4 ">
           <li>
-            <Link
-              href={"/post/new"}
-              className="px-5 py-2 text-white rounded-lg font-custom-bd bg-custom-blue button"
-            >
-              <Image
-                src={"/asset/image/modify-white.svg"}
-                alt=""
-                width={20}
-                height={20}
-              />
-              Write
-            </Link>
+            {isLogin && (
+              <Link
+                href={"/post/new"}
+                className="px-5 py-2 text-white rounded-lg font-custom-bd bg-custom-blue button"
+              >
+                <Image
+                  src={"/asset/image/modify-white.svg"}
+                  alt=""
+                  width={20}
+                  height={20}
+                />
+                Write
+              </Link>
+            )}
           </li>
           <li>
-            <button className="px-3 font-custom-bd button">
-              <Image
-                src="/asset/image/logout.svg"
-                alt=""
-                width={20}
-                height={20}
-              />
-              Logout
-            </button>
+            {!isLogin ? (
+              <Link href={"/login"} className="px-3 font-custom-bd button">
+                <Image
+                  src={"/asset/image/icon-login.svg"}
+                  alt=""
+                  width={20}
+                  height={20}
+                />
+                Login
+              </Link>
+            ) : (
+              <button className="px-3 font-custom-bd button" onClick={signOut}>
+                <Image
+                  src={"/asset/image/icon-logout.svg"}
+                  alt=""
+                  width={20}
+                  height={20}
+                />
+                Logout
+              </button>
+            )}
           </li>
         </ul>
       </div>
