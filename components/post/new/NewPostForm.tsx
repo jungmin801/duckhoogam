fe7f6094ef09f7616443374a75dd613c08b309fe
Jungmin Moon <jm.moon0801@gmail.com>
2024-02-18 20:50:49 +0900

    Refactor: 테이블 구조 변경에 따른 수정 테이블 구조 변경에 따라 로직 수정 Related to: #33

--- components/post/new/NewPostForm.tsx
+++ components/post/new/NewPostForm.tsx
@@ -1,108 +1,83 @@
 "use client";
-import React, { useRef, useState, Dispatch, SetStateAction } from "react";
+import React from "react";
 import BaseButton from "../../buttons/BaseButton";
 import Select from "../../common/Select";
 import Editor from "../../editor/Editor";
 import { Categories } from "../../../types/types";
+import { useForm } from "react-hook-form";
 
 const NewPostForm = ({ categories }: Categories) => {
-  const [content, setContent] = useState<string>("");
-  const [checkItems, setCheckItems] = useState<string[]>([]);
-  const [errors, setErrors] = useState({
-    titleError: false,
-    categoryError: false,
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+    getValues,
+    setValue,
+    trigger,
+  } = useForm({
+    defaultValues: {
+      categories: [],
+      title: "",
+      content: "",
+    },
+    mode: "onChange",
   });
-  const titleRef = useRef(null);
 
-  const checkTitleValid = (ref) => {
-    if (!ref.value) {
-      setErrors((prev) => ({
-        ...prev,
-        titleError: true,
-      }));
-      ref.focus();
-    } else {
-      setErrors((prev) => ({
-        ...prev,
-        titleError: false,
-      }));
-    }
-    return ref.value;
-  };
+  const handleSubmitPost = async (post) => {
+    console.log(post);
 
-  const checkCategoryValid = (arr) => {
-    if (arr.length === 0) {
-      setErrors((prev) => ({
-        ...prev,
-        categoryError: true,
-      }));
-    }
-
-    return arr;
-  };
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const titleData = checkTitleValid(titleRef.current);
-    const cateData = checkCategoryValid(checkItems);
-
-    const submitData = {
-      title: titleData,
-      content: content,
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/495px-No-Image-Placeholder.svg.png?20200912122019",
-      category: cateData,
-      userId: "Moonie",
-    };
-
-    if (titleData && cateData.length > 0) {
-      await fetch("/api/post/new", {
-        method: "POST",
-        body: JSON.stringify(submitData),
-      })
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-          console.log(data);
-        });
-    }
+    const response = await fetch("/api/post/new", {
+      method: "POST",
+      body: JSON.stringify(post),
+    });
   };
   return (
     <form
       className="bg-white w-[57rem] max-w-full mt-[-18rem] m-auto p-20 relative rounded-2xl pb-10"
-      onSubmit={(event) => {
-        handleSubmit(event);
-      }}
+      onSubmit={handleSubmit(handleSubmitPost)}
     >
       <div className="relative">
-        <input
-          type="text"
-          className={`w-full py-2 text-3xl font-custom-bd border-b focus:outline-none ${
-            errors.titleError && "border-custom-red"
-          }`}
-          placeholder="제목을 입력하세요"
-          ref={titleRef}
-        />
-        {errors.titleError && (
+        <label>
+          <input
+            type="text"
+            className={`w-full py-2 text-3xl font-custom-bd border-b focus:outline-none ${
+              errors.title && "border-custom-red"
+            }`}
+            placeholder="제목을 입력하세요."
+            {...register("title", {
+              required: "제목을 입력해주세요.",
+            })}
+          />
+        </label>
+        {errors.title && (
           <p className="absolute right-0 -translate-y-1/2 top-1/2 text-custom-red">
-            제목을 입력하세요
+            {errors.title.message}
           </p>
         )}
       </div>
       <div className={`relative flex content-between gap-10 py-1 my-8 `}>
         <strong className={`font-custom-bd shrink-0`}>카테고리</strong>
         <Select
+          control={control}
+          name="categories"
           categories={categories}
-          checkItems={checkItems}
-          setCheckItems={setCheckItems}
+          rules={{
+            validate: () => {
+              return (
+                getValues("categories").length > 0 ||
+                "하나 이상의 카테고리를 선택해주세요."
+              );
+            },
+          }}
         />
-        {errors.titleError && (
+        {errors.categories && (
           <p className="absolute bottom-0 right-0 text-custom-red">
             하나 이상의 카테고리를 선택하세요.
           </p>
         )}
       </div>
-      <Editor setContent={setContent} />
+      <Editor setValue={setValue} trigger={trigger} />
       <div className="flex justify-center gap-4 mt-8">
         <BaseButton isSubmit={false} txt={"Cancel"} isFilled={false} />
         <BaseButton isSubmit={true} txt={"Submit"} isFilled={true} />
